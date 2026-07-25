a6ff0d6916495b9c120379114f02c704fffe80d5
marvin <marvzknows03@gmail.com>
2026-07-25 23:27:05 +0800

    refactor(eslint): datatable eslint warning

--- src/components/data-table/data-table.tsx
+++ src/components/data-table/data-table.tsx
@@ -103,7 +103,11 @@ export function DataTable<TData>({
 
   function toggleRow(id: string) {
     const next = new Set(selected);
-    next.has(id) ? next.delete(id) : next.add(id);
+    if (next.has(id)) {
+      next.delete(id);
+    } else {
+      next.add(id);
+    }
     emitSelection(next);
   }
 
