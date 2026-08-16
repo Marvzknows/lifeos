4c018d6c792dcbeb22498cd282875c5a7dc4bfc5
marvin <marvzknows03@gmail.com>
2026-08-16 20:56:02 +0800

    feat(API): get task by ID api route

---
+++ src/app/api/tasks/[id]/route.ts
@@ -0,0 +1,19 @@
+import { getCurrentUserId } from "@/lib/auth/get-current-user-id";
+import { handleApiError } from "@/lib/server/errors/handle-api-error";
+import { getTaskById } from "@/lib/server/services/task-service";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  try {
+    const { id } = await params;
+    const userId = getCurrentUserId(request);
+
+    const task = await getTaskById(userId, id);
+    return NextResponse.json({ task }, { status: 200 });
+  } catch (error) {
+    return handleApiError(error);
+  }
+}
--- src/lib/server/services/task-service.ts
+++ src/lib/server/services/task-service.ts
@@ -1,4 +1,5 @@
 import prisma from "@/lib/prisma";
+import { ForbiddenError, NotFoundError } from "../errors/errors";
 
 type CreateTaskPayloadT = {
   title: string;
@@ -11,8 +12,51 @@ export async function createTask(userId: string, data: CreateTaskPayloadT) {
   });
 }
 
-// Get task
+export async function getTaskById(userId: string, id: string) {
+  const task = await prisma.task.findUnique({
+    where: { id },
+    select: {
+      id: true,
+      title: true,
+      completed: true,
+      priority: true,
+      dueDate: true,
+      createdAt: true,
+      updatedAt: true,
+      user: {
+        select: {
+          id: true,
+          email: true,
+          name: true,
+        },
+      },
+    },
+  });
+
+  if (!task) throw new NotFoundError("Task not found");
+  if (task.user.id !== userId) {
+    throw new ForbiddenError("You don't have permission to view this task");
+  }
+
+  const { user, ...rest } = task;
+  return { ...rest, created_by: user.name };
+}
 
 // Update task
+export async function updateTask(
+  userId: string,
+  id: string,
+  data: Partial<CreateTaskPayloadT>,
+) {
+  return prisma.task.update({
+    where: { id, userId },
+    data,
+  });
+}
 
 // Delete task
+export async function deleteTask(userId: string, id: string) {
+  return prisma.task.delete({
+    where: { id, userId },
+  });
+}
