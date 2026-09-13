b4abacc5986ca9096178ba2e7ff69596cf28dc64
marvin <marvzknows03@gmail.com>
2026-09-13 21:54:40 +0800

    feat(DB): transaction database table

---
+++ prisma/migrations/20260913135341_transaction_table/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" UUID NOT NULL,
+    "amount" DECIMAL(12,2) NOT NULL,
+    "description" TEXT,
+    "transactionDate" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+    "userId" UUID NOT NULL,
+    "categoryId" UUID NOT NULL,
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Transaction_userId_transactionDate_idx" ON "Transaction"("userId", "transactionDate");
+
+-- CreateIndex
+CREATE INDEX "Transaction_userId_categoryId_idx" ON "Transaction"("userId", "categoryId");
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
